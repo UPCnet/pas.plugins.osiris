eff33cdba6b7d4c9f41ea1fbdd5097bf2664c073
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-03-24 10:30:36 +0100

    Make the fallback to UPC oauth servers

--- pas/plugins/osiris/plugin.py
+++ pas/plugins/osiris/plugin.py
@@ -45,7 +45,11 @@ class OsirisHelper(BasePlugin, Cacheable):
             username = request.get_header('X-Oauth-Username')
             token = request.get_header('X-Oauth-Token')
             scope = request.get_header('X-Oauth-Scope')
-            data = dict(username=username, access_token=token, scope=scope)
+
+            if 'upc.edu' in self.oauth_server:  # Shame on you UPC!
+                data = dict(user_id=username, oauth_token=token, scope=scope)
+            else:
+                data = dict(username=username, access_token=token, scope=scope)
             req = requests.post("{}/checktoken".format(self.oauth_server), data=data)
             if req.status_code == 200:
                 creds['login'] = username
